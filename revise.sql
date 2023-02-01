8156db3e4bcd962051d5c61f25592aefa6d4cf5b
hasnat_osman <mdhasnatosman007@gmail.com>
2023-02-01 23:55:43 +0600

    updated

--- revise.sql
+++ revise.sql
@@ -10,3 +10,56 @@ from customers
 where state = "FL"
 order by points desc;
 
+select
+	first_name,
+    last_name,
+    points,
+    points + 10 as added_10
+from customers;
+
+select state
+from customers;
+
+select distinct(state)
+from customers;
+
+-- exercise: Return all the products name, unit price, new price ( unit price * 1.1)
+
+select
+	name,
+    unit_price,
+    unit_price * 1.1 as new_unit_price
+from products;
+
+-- where clause ........................................................ 
+
+select *
+from customers
+where points > 3000;
+
+select *
+from customers
+where state <> "VA";
+
+select *
+from customers
+where birth_date >  "1990-01-01";
+
+-- EXERCISE: Get the orders placed this year .................... 
+
+select *
+from orders
+where order_date >= "2019-01-01";
+
+
+select *
+from customers
+where birth_date > "1990-01-01" and points > 1000;
+
+select *
+from customers
+where birth_date > "1990-01-01" or 
+	(points > 1000 and state = "VA");
+
+
+
