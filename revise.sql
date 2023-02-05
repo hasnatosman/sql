6cd267597860e212e18507cbc41868e2c9a1b0e9
hasnat_osman <mdhasnatosman007@gmail.com>
2023-02-05 23:48:12 +0600

    update

--- revise.sql
+++ revise.sql
@@ -60,6 +60,11 @@ select *
 from customers
 where birth_date > "1990-01-01" or 
 	(points > 1000 and state = "VA");
+    
+
+select *
+from order_items
+where order_id = 6 and unit_price * quantity > 30;
 
 
 
